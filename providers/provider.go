7962dc9e172db239bf5e429fb8619da92d7640db
sravanth <55010766+allurisravanth@users.noreply.github.com>
2024-10-16 12:24:31 +0530

    Update provider.go

--- providers/provider.go
+++ providers/provider.go
@@ -42,6 +42,7 @@ func NewProviderRegistry(providerNames ...string) *ProviderRegistry {
 		"groq":      NewGroqProvider,
 		"ollama":    NewOllamaProvider,
 		"mistral":   NewMistralProvider,
+		"aicore":    NewAicoreProvider,
 		// Add other providers here as they are implemented
 	}
 
